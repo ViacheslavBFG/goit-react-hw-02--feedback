8bebcf67978891b0f034d2f8812cc3b4a87a8485
ViacheslavBFG <131298066+ViacheslavBFG@users.noreply.github.com>
2023-09-18 13:51:00 +0300

    1

--- src/components/module/Feedback/Feedback.jsx
+++ src/components/module/Feedback/Feedback.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
-
-export const Feedback = ({options, onLeaveFeedback}) => {
+export const Feedback = ({ options, onLeaveFeedback }) => {
   return (
     <div>
       {options.map(option => (
-        <button key={option} variant="secondary" onClick={() => onLeaveFeedback(option)}>
+        <button
+          key={option}
+          variant="secondary"
+          onClick={() => onLeaveFeedback(option)}
+        >
           {option}
         </button>
       ))}
     </div>
   );
 };
-
